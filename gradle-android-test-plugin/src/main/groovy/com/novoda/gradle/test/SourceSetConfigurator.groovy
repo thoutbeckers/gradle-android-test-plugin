b1d343972d1c9fa4f0f547ac9eb3205caccff019
frankiesardo <fran.sardo@gmail.com>
2013-10-11 16:25:32 +0100

    Correctly pass in the language as a parameter

--- gradle-android-test-plugin/src/main/groovy/com/novoda/gradle/test/SourceSetConfigurator.groovy
+++ gradle-android-test-plugin/src/main/groovy/com/novoda/gradle/test/SourceSetConfigurator.groovy
@@ -17,19 +17,19 @@ class SourceSetConfigurator {
         this.androidRuntime = androidRuntime
     }
 
-    public void configure(SourceSet variationSources, Task javaCompile, def testCompileClasspath,
+    public void configure(SourceSet variationSources, Task compileAndroid, FileCollection testCompileClasspath,
                           def testDestinationDir, String buildTypeName, List projectFlavorNames, String projectFlavorName) {
 
         variationSources.java.setSrcDirs testSrcDir(buildTypeName, projectFlavorName, projectFlavorNames, 'java')
-        Task testCompileTaskJava = compileTestTask(variationSources, javaCompile, testCompileClasspath, testDestinationDir, 'java')
+        Task testCompileTaskJava = compileTestTask(variationSources, variationSources.java, compileAndroid, testCompileClasspath, testDestinationDir, 'java')
 
         if (project.plugins.hasPlugin('groovy')) {
             variationSources.groovy.setSrcDirs testSrcDir(buildTypeName, projectFlavorName, projectFlavorNames, 'groovy')
-            Task testCompileTaskGroovy = compileTestTask(variationSources, testCompileTaskJava, testCompileClasspath, testDestinationDir, 'groovy')
+            Task testCompileTaskGroovy = compileTestTask(variationSources, variationSources.groovy, testCompileTaskJava, testCompileClasspath, testDestinationDir, 'groovy')
         }
     }
 
-    private Task compileTestTask(variationSources, Task compileAndroid, FileCollection testCompileClasspath, FileCollection testDestinationDir, String language) {
+    private Task compileTestTask(variationSources, compileSourceSet, Task compileAndroid, FileCollection testCompileClasspath, FileCollection testDestinationDir, String language) {
         // Create a task which compiles the test sources.
         def testCompileTask = project.tasks.getByName variationSources.getCompileTaskName(language)
         // Depend on the project compilation (which itself depends on the manifest processing task).
@@ -37,7 +37,7 @@ class SourceSetConfigurator {
         testCompileTask.group = null
         testCompileTask.description = null
         testCompileTask.classpath = testCompileClasspath
-        testCompileTask.source = language == 'java' ? variationSources.java : variationSources.groovy
+        testCompileTask.source = language == compileSourceSet
         testCompileTask.destinationDir = testDestinationDir.getSingleFile()
         testCompileTask.doFirst {
             testCompileTask.options.bootClasspath = androidRuntime
