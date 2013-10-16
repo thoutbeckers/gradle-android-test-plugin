5a577a2a782bd2bc34d4bc40687c5d28f79546c5
frankiesardo <fran.sardo@gmail.com>
2013-10-16 20:27:54 +0100

    Extract helper methods

--- gradle-android-test-plugin/src/main/groovy/com/novoda/gradle/test/AndroidTestPluginExtension.groovy
+++ gradle-android-test-plugin/src/main/groovy/com/novoda/gradle/test/AndroidTestPluginExtension.groovy
@@ -1,5 +1,4 @@
 package com.novoda.gradle.test
-
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
@@ -24,8 +23,8 @@ class AndroidTestPluginExtension {
 
         Project projectUnderTest = project.project(projectName)
 
-        def hasAppPlugin = projectUnderTest.plugins.hasPlugin "android"
-        def hasLibraryPlugin = projectUnderTest.plugins.hasPlugin "android-library"
+        def hasAppPlugin = hasAppPlugin(projectUnderTest)
+        def hasLibraryPlugin = hasLibraryPlugin(projectUnderTest)
 
         if (!hasAppPlugin && !hasLibraryPlugin) {
             throw new IllegalStateException("The 'android' or 'android-library' plugin is required.")
@@ -33,9 +32,46 @@ class AndroidTestPluginExtension {
             throw new IllegalStateException(
                     "Having both 'android' and 'android-library' plugin is not supported.")
         }
-        // Get the 'test' configuration for test-only dependencies.
-        Configuration testConfiguration = project.configurations.getByName('testCompile')
 
+        Configuration testConfiguration = makeTestConfiguration(projectUnderTest)
+
+        TestReport testTask = makeTestTask()
+
+        def variants = hasAppPlugin ? projectUnderTest.android.applicationVariants :
+                projectUnderTest.android.libraryVariants
+
+        // Configure every build variant
+        VariationConfigurator variantConfigurator = new VariationConfigurator(project, testConfiguration, testTask)
+        variants.all { variant ->
+            variantConfigurator.configure(variant)
+        }
+    }
+
+    private Configuration makeTestConfiguration(Project projectUnderTest) {
+        def androidPlugin = projectUnderTest.plugins.getPlugin(hasAppPlugin(projectUnderTest) ? "android" : "android-library")
+
+        def androidRuntime = project.files(androidPlugin.runtimeJarList)
+        def projectUnderTestDependencies = project.files(projectUnderTest)
+
+        // Add additional dependencies such as the project under test and the android runtime
+        project.dependencies {
+            testCompile androidRuntime
+            testCompile projectUnderTestDependencies
+        }
+
+        // Get the 'test' configuration for test-only dependencies.
+        project.configurations.getByName('testCompile')
+    }
+
+    private boolean hasLibraryPlugin(Project projectUnderTest) {
+        projectUnderTest.plugins.hasPlugin "android-library"
+    }
+
+    private boolean hasAppPlugin(Project projectUnderTest) {
+        projectUnderTest.plugins.hasPlugin "android"
+    }
+
+    private TestReport makeTestTask() {
         // Replace the root 'test' task for running all unit tests.
         Task testTask = project.tasks.replace(TEST_TASK_NAME, TestReport)
         testTask.destinationDir = project.file("$project.buildDir/$TEST_REPORT_DIR")
@@ -43,22 +79,7 @@ class AndroidTestPluginExtension {
         testTask.group = JavaBasePlugin.VERIFICATION_GROUP
         // Add our new task to Gradle's standard "check" task.
         project.tasks.check.dependsOn testTask
-
-
-        def androidPlugin = projectUnderTest.plugins.getPlugin(hasAppPlugin ? "android" : "android-library");
-
-        project.dependencies {
-            testCompile project.files(androidPlugin.runtimeJarList)
-            testCompile projectUnderTest
-        }
-
-        def variants = hasAppPlugin ? projectUnderTest.android.applicationVariants :
-                projectUnderTest.android.libraryVariants
-
-        VariationConfigurator variantConfigurator = new VariationConfigurator(project, testConfiguration, testTask)
-        variants.all { variant ->
-            variantConfigurator.configure(variant)
-        }
+        testTask
     }
 }
 
